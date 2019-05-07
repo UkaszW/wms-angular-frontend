ed380f6c38aa49eab002518467867d746ca8733b
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-05-07 23:43:49 +0200

    improve material forms items edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemEditComponent } from './item-edit/item-edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/item-list', pathMatch: 'full' },
+  {
+    path: 'item-list',
+    component: ItemListComponent
+  },
+  {
+    path: 'item-add',
+    component: ItemEditComponent
+  },
+  {
+    path: 'item-edit/:id',
+    component: ItemEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/item/item.service.ts
+++ src/app/shared/item/item.service.ts
@@ -7,10 +7,31 @@ import { Observable } from 'rxjs';
 })
 export class ItemService {
 
+  public API = '//localhost:8080';
+  public ITEM_API = this.API + '/items';
+
   constructor(private http: HttpClient) {
   }
 
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8080/items/get/all');
+    return this.http.get(this.ITEM_API + '/get/all');
+  }
+
+  get(id: string) {
+    return this.http.get(this.ITEM_API + '/' + id);
+  }
+
+  save(item: any): Observable<any> {
+    let result: Observable<Object>;
+    if (item['href']) {
+      result = this.http.put(item.href, item);
+    } else {
+      result = this.http.post(this.ITEM_API, item);
+    }
+    return result;
+  }
+
+  remove(href: string) {
+    return this.http.delete(href);
   }
 }
