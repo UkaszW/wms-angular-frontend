519557ef40a8d4dba958020120978e657fb7d33e
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-06-05 09:18:10 +0200

    test

--- src/app/item-edit/item-edit.component.spec.ts
+++ src/app/item-edit/item-edit.component.spec.ts
@@ -8,7 +8,7 @@ describe('ItemEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemEditComponent ]
+      declaration: [ ItemEditComponent ]
     })
     .compileComponents();
   }));
